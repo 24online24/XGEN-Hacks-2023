ff6176126ecb1ab69d6873313713582efa087058
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2023-11-24 01:30:32 +0200

    Change to the new translation for the book

--- load_data_vdb.py
+++ load_data_vdb.py
@@ -1,13 +1,12 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
-from langchain.document_loaders import UnstructuredHTMLLoader, BSHTMLLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import GPT4AllEmbeddings
-from langchain.embeddings import OllamaEmbeddings
 
+from translate import Translator
+import time
 
-DATA_PATH = "data_test/"
+DATA_PATH = "data/"
 DB_PATH = "vectorstores/db/"
 
 
@@ -15,23 +14,16 @@ def create_vector_db():
     loader = PyPDFDirectoryLoader(DATA_PATH)
     documents = loader.load()
     print(f"Processed {len(documents)} pdf files")
-    # documents = documents[:10]
-    # t5_small_pipeline = pipeline(
-    #     task="translation_ro_to_en",
-    #     model="t5-small",
-    #     max_length=1024,
-    #     model_kwargs={
-    #         "cache_dir": '/Users/rares/code/hacks2023/XGEN-Hacks-2023/t5_small'}
-    # )
+    translator_ro_to_en = Translator(to_lang="en", from_lang="ro")
 
-    # start_time = time.time()
-    # for idx, doc in enumerate(documents):
-    #     translated_page_content = t5_small_pipeline(doc.page_content)[0][
-    #         'translation_text']
-    #     doc.page_content = translated_page_content
-    #     if idx % 10 == 0:
-    #         print(f"{idx}- {time.time() - start_time} seconds")
-    #         start_time = time.time()
+    start_time = time.time()
+    for idx, doc in enumerate(documents):
+        translated_page_content = translator_ro_to_en.translate(
+            doc.page_content)
+        doc.page_content = translated_page_content
+        if idx % 10 == 0:
+            print(f"{idx}- {time.time() - start_time} seconds")
+            start_time = time.time()
 
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, chunk_overlap=50)
